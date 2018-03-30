ae8093ead43fe11bd7534b161baaf7464a2b5a84
joshroby <notify@joshroby.com>
2018-03-29 21:06:56 -0500

    Mar 29

--- model.js
+++ model.js
@@ -1,6 +1,9 @@
 var model = {
 
 	gameTitle: 'FALQSComm',
+	
+	supportLink: 'http://patreon.com/joshroby',
+	supportLinkLabel: 'Patreon',
 
 	gameDivContents: function() {
 	
@@ -165,14 +168,22 @@ function Game() {
 	};
 	
 	this.potentialAllies = [
-		{name:'Local Activist Network Providing Assistance Right To You',header:"Alliances",members: 100,allyCost:10,tickCostStat:'Money',tickCostNum:-5},
-		{name:'Queer Crossroads Shelter',header:"Alliances",members: 20,allyCost:10,tickCostStat:'Reputation',tickCostNum:-1},
-		{name:'Local Black Lives Matter',header:"Alliances",members: 200,allyCost:50,tickCostStat:'Reputation',tickCostNum:-2},
-		{name:'Audubon Society Local Chapter',header:"Alliances",members: 100,allyCost:20,tickCostStat:'Reputation',tickCostNum:1},
-		{name:'Showing Up for Racial Justice',header:"Alliances",members: 500000,allyCost:10000,tickCostStat:'Buzz',tickCostNum:-10},
-		{name:'National Black Lives Matter',header:"Alliances",members: 2000000,allyCost:50000,tickCostStat:'Reputation',tickCostNum:-100},
-		{name:'Federation of Labor Unions',header:"Alliances",members: 5000000,allyCost:150000,tickCostStat:'Buzz',tickCostNum:-100},
-		{name:'The Neoliberal Establishment',header:"Alliances",members: 10000000,allyCost:100000,tickCostStat:'People',tickCostNum:-100},
+		{name:'Local Activist Network Providing Assistance Right To You',header:"Alliances",members: 100,allyCost:100,tickCostStat:'Money',tickCostNum:-5},
+		{name:'Queer Crossroads Shelter',header:"Alliances",members: 20,allyCost:500,tickCostStat:'Reputation',tickCostNum:-1},
+		{name:'Local Black Lives Matter',header:"Alliances",members: 200,allyCost:1000,tickCostStat:'Reputation',tickCostNum:-2},
+		{name:'Audubon Society Local Chapter',header:"Alliances",members: 100,allyCost:10000,tickCostStat:'Reputation',tickCostNum:1},
+		{name:'Fight for $15',header:"Alliances",members: 500,allyCost:2000,tickCostStat:'Buzz',tickCostNum:-10},
+		{name:'Indigenous Environmental Network',header:"Alliances",members: 1000,allyCost:3000,tickCostStat:'Buzz',tickCostNum:-10},
+		{name:'Muslims for Progressive Values',header:"Alliances",members: 2000,allyCost:5000,tickCostStat:'Reputation',tickCostNum:-50},
+		{name:'Service Employees International Union, Local Chapter',header:"Alliances",members: 5000,allyCost:10000,tickCostStat:'Reputation',tickCostNum:-50},
+		{name:'Human Rights Campaign',header:"Alliances",members: 10000,allyCost:50000,tickCostStat:'Reputation',tickCostNum:-50},
+		{name:'Fair Immigration Reform Movement',header:"Alliances",members: 20000,allyCost:20000,tickCostStat:'Reputation',tickCostNum:-50},
+		{name:'Showing Up for Racial Justice',header:"Alliances",members: 500000,allyCost:100000,tickCostStat:'Buzz',tickCostNum:-10},
+		{name:'National Organization for Women',header:"Alliances",members: 1000000,allyCost:200000,tickCostStat:'Buzz',tickCostNum:-10},
+		{name:'National Black Lives Matter',header:"Alliances",members: 2000000,allyCost:500000,tickCostStat:'Reputation',tickCostNum:-100},
+		{name:'Federation of Labor Unions',header:"Alliances",members: 5000000,allyCost:1500000,tickCostStat:'Buzz',tickCostNum:-500},
+		{name:'GLAAD',header:"Alliances",members: 10000000,allyCost:1500000,tickCostStat:'Buzz',tickCostNum:-1000},
+		{name:'The Neoliberal Establishment',header:"Alliances",members: 50000000,allyCost:10000000,tickCostStat:'People',tickCostNum:-1000},
 	];
 	this.allies = [];
 
@@ -703,6 +714,8 @@ function Game() {
 			if (game.unlocked.IntegratedSocialMedia == true) {integratedsocialmedia = game.socialmediaambassadors};
 			this.update('Buzz',game.buzz * -0.1 / (1+integratedsocialmedia));
 			if (game.buzz < 10 && totalStaff > 0) {view.lowBuzzWarning();} else {view.clearReputationNotes();};
+		} else {
+			game.buzz = 0;
 		};
 		
 		// Allies Subsuming
@@ -824,7 +837,7 @@ function Game() {
 			view.update('Money');
 			view.update('Reputation');
 		};
-		if (this.reputation < 0) {
+		if (this.reputation < 0 && this.money == 0) {
 			if (totalStaff > 0) { // Don't pay your staff, lose people
 				this.update("People",(this.people-20) * -0.1);
 			};
